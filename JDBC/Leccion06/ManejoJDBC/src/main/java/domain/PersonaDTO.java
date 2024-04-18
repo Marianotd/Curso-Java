1def51dc3d0ba111dd21e5bf0caf11a85a216f51
MarianoTorres <marianotorresdistefano@gmail.com>
2024-04-18 11:16:49 -0300

    Pool de conexiones

---
+++ JDBC/Leccion06/ManejoJDBC/src/main/java/domain/PersonaDTO.java
@@ -0,0 +1,88 @@
+package domain;
+
+import java.io.Serializable;
+
+public class PersonaDTO implements Serializable {
+
+    private int idPersona;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public PersonaDTO() {
+    }
+
+    public PersonaDTO(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public PersonaDTO(String nombre, String apellido, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public PersonaDTO(int idPersona, String nombre, String apellido, String email, String telefono) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public int getIdPersona() {
+        return this.idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return this.apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return this.telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("idPersona=").append(this.idPersona);
+        sb.append(", nombre=").append(this.nombre);
+        sb.append(", apellido=").append(this.apellido);
+        sb.append(", email=").append(this.email);
+        sb.append(", telefono=").append(this.telefono);
+        sb.append('}');
+        return sb.toString();
+    }
+
+}
