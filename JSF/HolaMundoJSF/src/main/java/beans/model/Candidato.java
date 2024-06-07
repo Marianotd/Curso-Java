2a12fda89676d2b780a67030514de191bba5b3d5
MarianoTorres <marianotorresdistefano@gmail.com>
2024-06-07 12:53:02 -0300

    JSF

---
+++ JSF/HolaMundoJSF/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,20 @@
+package beans.model;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class Candidato {
+
+    private String nombre = "Introduce tu nombre";
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
